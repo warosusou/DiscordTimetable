d6f05963df98f0831fcb52b9247add14117b6702
warosusou <syunsuke1999@hotmail.co.jp>
2020-05-11 20:14:52 +0900

    DiscordにRPCを表示する部分が完成

--- DiscordTimetable/DiscordTimetable/Form1.cs
+++ DiscordTimetable/DiscordTimetable/Form1.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,17 +18,31 @@ namespace DiscordTimetable
     {
         Dictionary<DayOfWeek, Dictionary<int, string>> table = null;
         const string path = "timeSchedule.json";
+        DiscordRpcClient client;
+
         public Form1()
         {
             InitializeComponent();
             LoadTimeSchedule();
+            client = new DiscordRpcClient("709092892525985842");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.OnReady += (sender, e) =>
+            {
+                Console.WriteLine("Received Ready from user {0}", e.User.Username);
+            };
+
+            client.OnPresenceUpdate += (sender, e) =>
+            {
+                Console.WriteLine("Received Update! {0}", e.Presence);
+            };
+            client.Initialize();
         }
 
         private void GetTimeSchedule()
         {
             var s = new SeleniumingScomb();
             table = s.GetTimeTable();
-            if(table != null)
+            if (table != null)
             {
                 File.WriteAllText(path, JsonConvert.SerializeObject(table));
             }
@@ -55,18 +71,31 @@ namespace DiscordTimetable
             timer1.Enabled = true;
         }
 
+        private void SetDiscordStatus(string state)
+        {
+            var t = new Timestamps();
+            client.SetPresence(new RichPresence()
+            {
+                State = state
+            }) ;
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Enabled = false;
             var d = new DateTime(1, 1, 1, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            //var d = new DateTime(1, 1, 1, 9, 44, 0);
             for (int i = 0; i < TimeSchedule.StartingTimes.Count; i++)
             {
                 var t = TimeSchedule.StartingTimes.ElementAt(i);
                 if (d > t && t.AddMinutes(TimeSchedule.ClassInterval) > d)
                 {
                     table.TryGetValue(DateTime.Now.DayOfWeek, out var dic);
+                    //table.TryGetValue(DayOfWeek.Monday, out var dic);
                     dic.TryGetValue(i + 1, out var title);
+
                     label1.Text = title;
+                    SetDiscordStatus(title);
                 }
             }
             timer1.Enabled = true;
@@ -76,10 +105,15 @@ namespace DiscordTimetable
         {
             timer1.Enabled = false;
             GetTimeSchedule();
-            if(table != null)
+            if (table != null)
             {
                 LoadTimeSchedule();
             }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            client.Dispose();
         }
     }
 }
